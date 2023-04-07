094ff33642b2ba69532cffdcf9b00471ce417f66
Jack <jackhasakeyboard@gmail.com>
2023-04-07 04:05:58 -0600

    Generated list item

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
+#include <stdio.h>
+
+const char ws = ' ';
+
+const char prefix = '-';
+
+void err(char* msg) {
+	printf("Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	char* str = argv[1];
+
+	printf("%c%c%s\n", prefix, ws, str);
+
 	return 0;
 }
